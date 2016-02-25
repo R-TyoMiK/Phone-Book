ad56e82eb57c089d20dcf6ba5c1ff618859ae077
softmartian <softmartian@gmail.com>
2016-02-25 21:15:57 +0300

    + sorting + saveFile

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -11,10 +10,25 @@ public class Main {
     public static int MAX = 1000;
 
     public static void main(String[] args) throws IOException, NullPointerException {
+        /*ObjectInputStream objectInputStream = new ObjectInputStream(
+        * new FileInputStream("db.data"));
+        * String[] names*/
+        /*File file = new File ("data");
+        if (file.exists()) {
+            ObjectInputStream objectInputStream = new ObjectInputStream(
+                    new FileInputStream("db.data"));
+            names = (String[]) objectInputStream.readObject();
+        } else {
+            names = new String[MAX];
+        }*/
         Scanner in = new Scanner(System.in);
         String[] names = new String[MAX];
         String[] numbers = new String[MAX];
         boolean[] ids = new boolean[MAX];
+        int[] years = new int[MAX];
+        int[] months = new int[MAX];
+        int[] days = new int[MAX];
+
 
         System.out.println("Здравствуйте! Вас приветствует телефонный справочник.");
         System.out.println("Выберите нужный пункт в меню для дальнейших действий: ");
@@ -34,22 +48,22 @@ public class Main {
                 break;
             }
             if ("1".equals(menu)) {
-                addNewContact(ids, names, numbers);
+                addNewContact(ids, names, numbers, years, months, days);
             }
             if ("2".equals(menu)) {
                 removeContact(ids);
             }
             if ("4".equals(menu)) {
-                printOutContacts(ids, names, numbers);
+                printOutContacts(ids, names, numbers, years, months, days);
             }
             if ("5".equals(menu)) {
-                searchContactInPhoneBook(ids, names, numbers);
+                searchContactInPhoneBook(ids, names, numbers, years, months, days);
             }
             if ("6".equals(menu)) {
-                contactSorting(ids, names, numbers);
+                contactSorting(ids, names, numbers, years, months, days);
             }
             if ("7".equals(menu)) {
-                editContact(ids, names, numbers);
+                editContact(ids, names, numbers, years, months, days);
             } else {
                 System.out.println("Неверный пункт меню");
             }
@@ -66,20 +80,30 @@ public class Main {
         return -1;
     }
 
-    public static void addNewContact(boolean[] ids, String[] names, String[] numbers) {
+    public static void addNewContact(boolean[] ids, String[] names, String[] numbers, int[] years, int[] months, int[] days) {
         Scanner in = new Scanner(System.in);
         System.out.println("Добавление...");
         System.out.println("Введите имя:");
         String name = in.nextLine();
         System.out.println("Введите телефон абонента " + name + ":");
         String number = in.nextLine();
+        System.out.println("Год рождения:");
+        int year = in.nextInt();
+        System.out.println("Месяц рождения:");
+        int month = in.nextInt();
+        System.out.println("День рождения:");
+        int day = in.nextInt();
+
         int id = findId(ids);
         names[id] = name;
         numbers[id] = number;
+        years[id] = year;
+        months[id] = month;
+        days[id] = day;
         ids[id] = true;
     }
 
-    public static void printOutContacts(boolean[] ids, String[] names, String[] numbers) {
+    public static void printOutContacts(boolean[] ids, String[] names, String[] numbers, int[] years, int [] months, int[] days) {
         System.out.println("Список всех:");
         System.out.println("_______________________________");
         System.out.println("|" + "ID" + "|" + "   NAME   " + "|" + "   NUMBER   " + "|");
@@ -92,7 +116,7 @@ public class Main {
         }
     }
 
-    public static void searchContactInPhoneBook(boolean[] ids, String[] names, String[] numbers) {
+    public static void searchContactInPhoneBook(boolean[] ids, String[] names, String[] numbers, int[] years, int[] months, int[] days) {
         System.out.println("Поиск...");
         System.out.println("Выберите метод поиска: ");
         System.out.println("1 - по ID");
@@ -160,28 +184,82 @@ public class Main {
         }
     }
 
-    public static void contactSorting(boolean[] ids, String[] names, String[] numbers) {
+    public static void contactSorting(boolean[] ids, String[] names, String[] numbers, int[] years, int[] months, int[] days) throws IOException {
         System.out.println("Сортировка...");
-        System.out.println("Выберите тип сортировки:");
-        System.out.println("1 - по имени");
-        System.out.println("2 - по номеру телефона");
-        Scanner in = new Scanner(System.in);
-        String sorting = in.nextLine();
-        if ("1".equals(sorting)) {
-            Arrays.sort(names);
-            for (int i = 1; i < ids.length; i++) {
-                System.out.print(names[i] + "   ");
+        String[] nameSearch = new String[ids.length];
+        int minI = 1;
+        int k = 1;
+        for (int i = 1; i < ids.length; i++) {
+            if (ids[i]) {
+                for (int i1 = 1; i1 < ids.length; i1++) {
+                    if (ids[i1] && !findIn(names[i1], nameSearch)) {
+                        minI = i1;
+                        break;
+                    }
+                }
+                for (int j = 1; j < ids.length; j++) {
+                    if (ids[j] && !findIn(names[j], nameSearch) && names[j].compareTo(names[minI]) <= 0) {
+                        minI = j;
+                    }
+                }
+                nameSearch[k] = names[minI];
+                k++;
+                System.out.println(minI + " " + names[minI] + " " + numbers[minI]);
+
+
             }
         }
-        if ("2".equals(sorting)) {
-            Arrays.sort(numbers);
-            for (int i = 1; i < ids.length; i++) {
-                System.out.print(numbers[i] + "   ");
+        for (int i = 1; i < ids.length; i++) {
+            if (ids[i]) {
+                for (int i1 = 1; i1 < ids.length; i1++) {
+                    if (ids[i1] && !findIn(dateToString(years[i1], months[i1], days[i1]), nameSearch)) {
+                        minI = i1;
+                        break;
+                    }
+                }
+                for (int j = 1; j < ids.length; j++) {
+                    if (ids[j] && !findIn(dateToString(years[j], months[j], days[j]), nameSearch) &&
+                            dateToString(years[j], months[j], days[j]).
+                                    compareTo(dateToString(years[minI], months[minI], days[minI])) <= 0) {
+                        minI = j;
+                    }
+                }
+                nameSearch[k] = dateToString(years[minI], months[minI], days[minI]);
+                k++;
+                System.out.println(minI + " " + names[minI] + " " + dateToString(years[minI], months[minI], days[minI]));
+
+
             }
+
         }
+        saveToFile(names, "filename");
     }
 
-    public static void editContact(boolean[] ids, String[] names, String[] numbers) {
+
+    public static boolean findIn(String s, String[] m) {
+        for (int i = 1; i < m.length; i++) {
+            if (s.equals(m[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String dateToString(int year, int month, int date) {
+        String s = year + "-";
+        if (String.valueOf(month).length() == 1) {
+            s += "0";
+        }
+        s += month + "-";
+        if (date < 10) {
+            s += "0";
+        }
+        s += date;
+        return s;
+    }
+
+
+    public static void editContact(boolean[] ids, String[] names, String[] numbers, int[] years, int[] months, int[] days) {
         Scanner in = new Scanner(System.in);
         System.out.println("Редактирование...");
         System.out.println("Введите ID котакта для редактирования");
@@ -195,8 +273,19 @@ public class Main {
                 String newName = in.nextLine();
                 System.out.println("Введите новый телефон абонента");
                 String newNumber = in.nextLine();
+                int id = findId(ids);
+                names[id] = newName;
+                numbers[id] = newNumber;
+                ids[id] = true;
             }
         }
+    }
+
+    public static void saveToFile(String[] m, String fileName) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                new FileOutputStream(fileName));
+        objectOutputStream.writeObject(m);
+        objectOutputStream.close();
     }
 }
 
